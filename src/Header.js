e3e4509328b8dd9d047f09978c7df39bbe953056
jacinta246 <jacinta246@yahoo.com>
2020-11-13 04:09:25 -0500

    another commit just to make sure

--- src/Header.js
+++ src/Header.js
@@ -4,8 +4,9 @@ import logo from './logo.svg'
 
 const Header = () => (
     <div>
-    <img src={"https://www.flaticon.com/svg/static/icons/svg/957/957233.svg"} className="App-logo" alt="logo" />
-    <h1 className="App-title">SureFire Studios</h1>
+     {/* <size = "small" />    */}
+    <img src={"https://www.flaticon.com/svg/static/icons/svg/957/957233.svg"} className="App-logo" alt="logo"   />
+    <h2 className="App-title">SureFire Studios</h2>
     </div>
 
 )
